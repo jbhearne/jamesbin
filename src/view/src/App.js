d710eb7eaf37521aba122cefbe6e7e7c990d4811
JBHearne <95382527+jbhearne@users.noreply.github.com>
2023-02-07 14:38:06 -0600

    more structure and setup initial routes

--- src/view/src/App.js
+++ src/view/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>James' Bin</h1>
+        <div id="user-login">LOGIN</div>
+        <nav id="main-nav">
+          <ul>
+            <li><NavLink>Products</NavLink></li>
+            <li><NavLink>Vendors</NavLink></li>
+            <li><NavLink>Orders</NavLink></li>
+            <li><NavLink>Cart</NavLink></li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <Router>
+          <Routes>
+            <Route path='/' component={} />
+            <Route path='/products' component={} />
+            <Route path='/product/:id' component={} />
+            <Route path='/vendors' component={} />
+            <Route path='/vendor/:id' component={} />
+            <Route path='/orders' component={} />
+            <Route path='/order/checkout' component={} />
+            <Route path='/order/complete' component={} />
+            <Route path='/order/:id' component={} />
+            <Route path='/cart' component={} />
+            <Route path='/checkout' component={} />
+            <Route path='/user/register' component={} />
+            <Route path='/user/login' component={} />
+            <Route path='/user' component={} />
+          </Routes>
+        </Router>
+      </main>
     </div>
   );
 }
